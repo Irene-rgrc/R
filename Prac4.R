2c077c3eae83c70fed629f8f7529aec7a7bfc10f
Irene <ireneneri001@gmail.com>
2021-12-13 10:15:42 +0100

    Create Prac4.R

---
+++ Prac4.R
@@ -0,0 +1,10 @@
+###################################################
+# Irene Rodríguez García
+# Practica grafos
+# 13/12/2021
+##################################################
+
+#Llamamos a la librerias
+library(igraph)
+library(dplyr)
+library(ggplot2)
